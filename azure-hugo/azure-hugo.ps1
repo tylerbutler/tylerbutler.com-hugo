2d6f462c623333c5565d88162f194a2b5c28743d
Tyler Butler <tyler@tylerbutler.com>
2018-03-10 19:12:50 -0700

    Another deployment fix.

--- azure-hugo/azure-hugo.ps1
+++ azure-hugo/azure-hugo.ps1
@@ -1,4 +1,4 @@
-$hugoVersion = "0.31.1"
+$hugoVersion = "0.37.1"
 $hugoDownloadPath = "https://github.com/gohugoio/hugo/releases/download/v{0}/hugo_{0}_Windows-64bit.zip" -f $hugoVersion
 
 function Get-HugoPath($version=$hugoVersion) {
@@ -22,11 +22,14 @@ function Install-Hugo($version=$hugoVersion, $force=$false) {
 		d:\7zip\7za x hugo.zip
 		Write-Output "Done!"
 		Pop-Location
+	} else {
+		Write-Output "Skipping Hugo installation."
 	}
 }
 
-function Invoke-SiteBuild() {
+function Invoke-SiteBuild($version=$hugoVersion) {
 	# Build the site
+	$hugoPath = Get-HugoPath($version)
 	Push-Location $hugoPath
 	Write-Output "Building site..."
 	./hugo.exe -d D:\home\site\wwwroot
