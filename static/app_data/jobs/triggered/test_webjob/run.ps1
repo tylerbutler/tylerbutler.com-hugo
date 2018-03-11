037725aa0abfa5614ba07ed54617c8b00f146eaa
Tyler Butler <tyler@tylerbutler.com>
2018-03-10 19:24:58 -0700

    Update webjob

--- static/app_data/jobs/triggered/test_webjob/run.ps1
+++ static/app_data/jobs/triggered/test_webjob/run.ps1
@@ -1,3 +1,5 @@
-. D:\home\site\wwwroot\azure-hugo\azure-hugo.ps1
+. 'D:\home\site\wwwroot\azure-hugo\azure-hugo.ps1'
 
+Write-Output "Inside run.ps1..."
 Invoke-SiteBuild
+Write-Output "Exiting run.ps1"
